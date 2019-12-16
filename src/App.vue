f079875ea7138d995908f97d89ebedc5e2e360ac
usoghisa <usoghisa@users.noreply.github.com>
2019-12-16 21:42:41 +0000

    Add bulma

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome ugo to Your Vue.js App"/>
+    <HelloWorld msg="Welcome ugo vue-bulma  to Your Vue.js App"/>
   </div>
 </template>
 <script>
